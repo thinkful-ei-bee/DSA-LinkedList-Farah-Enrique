d2fe0bfb50721b5caaea02058f258f1a8a7ec699
Enrique Montemayor <eemontemayor@live.com>
2019-05-16 14:12:43 -0500

    finished findPrevious

--- linkedlist.js
+++ linkedlist.js
@@ -142,27 +142,6 @@ class LinkedList {
     previousNode.next = currNode.next;
   }
 }
+module.exports = LinkedList;
 
 
-function main(){
-  let SLL = new LinkedList();
-
-  SLL.insertFirst('Apollo');
-  SLL.insertLast('Boomer');
-  SLL.insertLast('Helo');
-  SLL.insertLast('Husker');
-  SLL.insertLast('Starbuck');
-  SLL.insertLast('Tauhida');
-
-  SLL.find('Tauhida');
-  // SLL.remove('squirrel');
-
-  SLL.insertBefore('Athena', 'Boomer')
-  SLL.insertAfter('Hotdog', 'Helo');
-
-  SLL.insertAt('Kat', 3)
-  SLL.remove('Tauhida')
-  console.log(SLL.find('Tauhida'));
-}
-
-main();
---
+++ suppFunctions.js
@@ -0,0 +1,77 @@
+const LinkedList = require('./LinkedList');
+const SLL = new LinkedList();
+const _Node = require('./linkedlist.js')
+const Node = new _Node();
+
+
+
+
+function main(){
+  
+  
+    SLL.insertFirst('Apollo');
+    SLL.insertLast('Boomer');
+    SLL.insertLast('Helo');
+    SLL.insertLast('Husker');
+    SLL.insertLast('Starbuck');
+    SLL.insertLast('Tauhida');
+  
+    SLL.find('Tauhida');
+    
+  
+    SLL.insertBefore('Athena', 'Boomer')
+    SLL.insertAfter('Hotdog', 'Helo');
+  
+    SLL.insertAt('Kat', 3)
+    SLL.remove('Tauhida')
+  
+  }
+  
+  main();
+
+function display(list){
+let currNode = list.head
+    while(currNode !== null){
+        console.log(currNode.value)
+        currNode = currNode.next
+    }
+}
+
+display(SLL)
+
+function size(list){
+    let currNode = list.head
+    counter = 0
+    while(currNode !== null){
+        currNode = currNode.next;
+        counter ++
+    }
+    console.log(counter)
+}
+size(SLL)
+
+function isEmpty(list){
+    if (list.head === null){
+        console.log('List is empty')
+    } else {
+        console.log('List is populated')
+    }
+}
+
+isEmpty(SLL)
+
+function findPrevious(list, item){
+    let currNode = list.head
+  
+    while(currNode.next !== null){
+        if( currNode.next.value === item){
+            console.log(currNode.value)
+        } 
+            currNode = currNode.next;
+
+        
+    }
+
+
+}
+findPrevious(SLL, 'Kat')
